0bf5359f876d8f1c3c85f5c83526c17172bf6370
nguyenanhtuVMO <nguyenanhtu.vmo@gmail.com>
2022-06-23 09:42:54 +0700

    up

---
+++ module1.py
@@ -0,0 +1,19 @@
+import cv2
+import numpy
+
+image = cv2.imread ('xedinguocchieu_1.jpg')
+blur_image = cv2.blur(image, (23, 23))
+
+mask = numpy.zeros(image.shape, dtype = numpy.unit8)
+mask = cv2.cvtColor(mask, cv2.COLOR_BG2GRAY)
+cv2.square(mask, (150,100), 50, 255, -1)
+front_image= cv2.bitwise_and(blur_image, blur_image, mask = mask)
+
+mask_inv = cv2.bitwise_not(mask)
+back_image = cv2.bitwise_and(image, image, mask = mask.inv)
+
+cv2.imshow('Car', front_image + back_image)
+cv2.waitKey(0)
+
+
+

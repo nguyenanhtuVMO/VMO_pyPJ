abc4665f8ab11200c873b70bd677adf3e21028e2
nguyenanhtuVMO <nguyenanhtu.vmo@gmail.com>
2022-08-16 10:35:30 +0700

    up

--- image processing.py
+++ image processing.py
@@ -286,7 +286,23 @@ for testing_name in test_labels:
         print(y_pred)
         print("Result: ", (y_pred == y_result).tolist().count(True)/len(y_result))
 
+def fd_haralick(iamge) :
+    # convert imgae to grayscale
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    # compute the haralick texture feature vector
+    haralick = mahotas.feature.haralick(gray).mean(axis = 0)
+    # return the result
+    return haralick
 
+def fd_histogram(image, mask = none):
+    # convert image to HSV color-space
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+    # compute the color histogram
+    hist = cv2.calcHist([image], [0,1,2], None , [bins, bins, bins], [0, 256, 0, 256, 0, 256])
+    # normalize the histogram
+    cv2.normalize(hist, hist)
+    # return the result
+    return hist.flatten()
 
 
 

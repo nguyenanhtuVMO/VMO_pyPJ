62c1188d0b46f3f5fdd617bc6594c90280300262
nguyenanhtuVMO <nguyenanhtu.vmo@gmail.com>
2022-08-17 13:46:27 +0700

    up

--- cv2.py
+++ cv2.py
@@ -18,3 +18,25 @@ back_image = cv2.bitwise_and(image, image, mask = mask.inv)
 
 cv2.imshow('Car', front_image + back_image)
 cv2.waitKey(0)
+
+def fd_haralick(image):
+    # convert the image to gray
+     gray = cv2.cvtColor(image, cv2.COLOR_BG2GRAY)
+     haralick = mahotas.feature.haralick(gray).mean(axis = 0)
+     # return the result
+     return haralick
+
+def fd_histogram(image, mask = None):
+    # convert the image to HSV space
+     image = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+     hist = cv2.CalcHist([image], [0, 1, 2], None, [bins, bins, bins], [0, 256, 0, 256, 0, 256])
+     # normalize the histogram
+     cv2.nomalize(hist, hist)
+     return hist.flatten()
+
+def fd_hu_moments(image):
+    # convert the image to gray
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    feature = cv2.HuMoments(cv2.moments(image).flatten)
+    # return the result
+    return feature

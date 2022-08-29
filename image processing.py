01792a40a0ba55fbe9a46eea649db9c8f5f2c5e7
nguyenanhtuVMO <nguyenanhtu.vmo@gmail.com>
2022-08-29 13:17:05 +0700

    up

--- image processing.py
+++ image processing.py
@@ -1,9 +1,10 @@
 from mpl_toolkits.mplot3d import Axes3D
 from sklearn.preprocessing import StandardScaler
-import matplotlib.pyplot as plt # plotting
-import numpy as np # linear algebra
-import os # accessing directory structure
-import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
+import matplotlib.pyplot as plt 
+import numpy as np 
+import pandas as pd 
+from scipy.ndimage.filters import maximum_filter
+from scipy.ndimage.morphology import generate_binary_structure, binary_erosion
 
 def fd_histogram(image, mask=None):
     # chuyển về không gian màu HSV
@@ -112,10 +113,48 @@ print ("[STATUS] training Labels {}".format(np.array(labels).shape))
 # encode the target labels
 le = LabelEncoder()
 target = le.git_transform(labels)
+                                      
+def detect_peaks(image):
+    """
+    Takes an image and detect the peaks usingthe local maximum filter.
+    Returns a boolean mask of the peaks (i.e. 1 when
+    the pixel's value is the neighborhood maximum, 0 otherwise)
+    """
 
-# normalize the feature vector in range (0-1)
-scaler = MinMaxScaler(feature_range=(0, 1))
-rescaled_feature = scaler.fit_transform(global_features)
+    # define an 8-connected neighborhood
+    neighborhood = generate_binary_structure(2,2)
+
+    #apply the local maximum filter; all pixel of maximal value 
+    #in their neighborhood are set to 1
+    local_max = maximum_filter(image, footprint=neighborhood)==image
+    #local_max is a mask that contains the peaks we are 
+    #looking for, but also the background.
+    #In order to isolate the peaks we must remove the background from the mask.
+
+    #we create the mask of the background
+    background = (image==0)
+
+    #a little technicality: we must erode the background in order to 
+    #successfully subtract it form local_max, otherwise a line will 
+    #appear along the background border (artifact of the local maximum filter)
+    eroded_background = binary_erosion(background, structure=neighborhood, border_value=1)
+
+    #we obtain the final mask, containing only peaks, 
+    #by removing the background from the local_max mask (xor operation)
+    detected_peaks = local_max ^ eroded_background
+
+    return detected_peaks
+
+
+#applying the detection and plotting results
+for i, paw in enumerate(paws):
+    detected_peaks = detect_peaks(paw)
+    pp.subplot(4,2,(2*i+1))
+    pp.imshow(paw)
+    pp.subplot(4,2,(2*i+2) )
+    pp.imshow(detected_peaks)
+
+pp.show()
 
 # save the feature vector using HDF5
 h5f_data = h5py.File(output_path+'data.h5', 'w')
@@ -287,7 +326,7 @@ for testing_name in test_labels:
         print("Result: ", (y_pred == y_result).tolist().count(True)/len(y_result))
 
 def fd_haralick(iamge) :
-    # convert imgae to grayscale
+    # convert imgae to grayscale 
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     # compute the haralick texture feature vector
     haralick = mahotas.feature.haralick(gray).mean(axis = 0)
